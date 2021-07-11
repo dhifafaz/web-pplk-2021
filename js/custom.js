24053fe7b796c693420e91c6100697a8b8bc4238
dhifafaz <dhifaf.az@gmail.com>
2021-07-11 13:46:06 +0700

    add footer bar, index page(home)

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,12 @@ document.addEventListener('DOMContentLoaded', (e) => {
         login.classList.add('display')
     }, 4000);
 })
+
+jQuery(function($) {
+    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    $('ul a').each(function() {
+        if (this.href === path) {
+            $(this).addClass('active');
+        }
+    });
+});
