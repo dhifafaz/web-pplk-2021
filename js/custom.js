88bdb3d175cf46264b86b1dd600448e985608baa
dhifafaz <dhifaf.az@gmail.com>
2021-07-16 06:38:20 +0700

    revisi dikir

--- js/custom.js
+++ js/custom.js
@@ -27,3 +27,23 @@ $(window).scroll(function() {
     }
 });
 
+
+// var clickedButtonJurusan = false;
+
+// document.getElementsByClassName('button-jurusan').addEventListener("click", function() {
+//     clickedButtonJurusan = true;
+// }​);
+
+// if (clickedButtonJurusan) {
+
+// }
+​
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
