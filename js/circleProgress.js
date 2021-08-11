46aff1fe9295eec1f060ddeff81cbf67baa0fccb
hfdzz <yhafidz@gmail.com>
2021-08-11 11:33:10 +0700

    question-section timer

--- js/circleProgress.js
+++ js/circleProgress.js
@@ -1,42 +1,35 @@
 const pi = Math.PI
-        var cnv = document.getElementById('result-canvas')
-        var cnvPercent = document.getElementById('canvas-percent')
-        var c = cnv.getContext('2d')
-        var posX = cnv.width/2
-        var posY = cnv.height/2
-        var percent = 100
-        var radPercent = 360/100 // Persentase per 360
-        var res = radPercent * 64
+var cnv = document.getElementById('question-canvas')
+var cnvTime = document.getElementById('canvas-time')
+var c = cnv.getContext('2d')
+var posX = cnv.width/2
+var posY = cnv.height/2
+var time = 20
+var degPerSec = 360/20
+var fps = 10 // Set FPS disini, default = 10
+var intervalTime = 1000/fps
 
-        c.lineCap = "round"
-        arcMove()
+c.lineCap = "round"
+arcMove()
 
-        function arcMove(){
-            var deg = 0
-            var arcInterval = setInterval(function(){
+function arcMove(){
+    var deg = 360
+    var arcInterval = setInterval(function(){
 
-                percent--
-                c.clearRect(0,0,cnv.width,cnv.height)
-                // percent = deg/radPercent
-                deg = percent*radPercent
-                cnvPercent.innerHTML = percent.toFixed()
+        deg -= degPerSec/fps
+        c.clearRect(0,0,cnv.width,cnv.height)
+        time = deg/degPerSec
+        cnvTime.innerHTML = time.toFixed()
 
-                // Draw Rail
-                c.beginPath()
-                c.arc(posX,posY,69,(pi/180)*270,(pi/180)*(270+360))
-                c.strokeStyle = '#63BCF4'
-                c.lineWidth = '2'
-                c.stroke()
+        // Draw Progress
+        c.beginPath()
+        c.arc(posX,posY,27,(pi/180)*270,(pi/180)*(270+deg))
+        c.strokeStyle = '#479FD7'
+        c.lineWidth = '4'
+        c.stroke()
 
-                // Draw Progress
-                c.beginPath()
-                c.arc(posX,posY,69,(pi/180)*270,(pi/180)*(270+deg))
-                c.strokeStyle = '#479FD7'
-                c.lineWidth = '5'
-                c.stroke()
-
-                if( deg <= 0){
-                    clearInterval(arcInterval)
-                }
-            },1000)
+        if( deg <= 0){
+            clearInterval(arcInterval)
         }
+    },intervalTime)
+}
